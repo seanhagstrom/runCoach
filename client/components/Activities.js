4432b5067a5abd8bee6d31e98a051d0207484223
Sean Hagstrom <seanhagstrom1@gmail.com>
2021-12-15 17:37:04 -0500

    rendered map of current location in component

--- client/components/Activities.js
+++ client/components/Activities.js
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from "react";
 import { connect } from "react-redux";
 import { getActivities } from "../store/activities";
 import Activity from "./Activity";
+// import L from "leaflet";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 
 export const Activities = (props) => {
   const [activities, setActivities] = useState([]);
@@ -20,9 +22,42 @@ export const Activities = (props) => {
     setActivities(props.activities);
   }, [props.activities]);
 
+  // var map = L.map("map").setView([51.505, -0.09], 13);
+
+  // L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  //   attribution:
+  //     '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+  // }).addTo(map);
+
+  function getLocation() {
+    navigator.geolocation.getCurrentPosition(showPos);
+  }
+
+  function showPos({ coords }) {
+    return coords;
+  }
+
+  getLocation();
   return (
     <div>
       <h3>Activities will be here</h3>
+      <div id='map'>
+        <MapContainer
+          center={[35.9563566, -84.0722167]}
+          zoom={11}
+          scrollWheelZoom={false}
+        >
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+          />
+          <Marker position={[51.505, -0.09]}>
+            <Popup>
+              A pretty CSS3 popup. <br /> Easily customizable.
+            </Popup>
+          </Marker>
+        </MapContainer>
+      </div>
       <table>
         <tbody>
           <tr>
