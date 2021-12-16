15cfb890d857b4704f85543fbbe50cae474dc783
Sean Hagstrom <seanhagstrom1@gmail.com>
2021-12-15 22:51:02 -0500

    rendered each activity to display on map

--- client/components/Activities.js
+++ client/components/Activities.js
@@ -2,8 +2,15 @@ import React, { useState, useEffect } from "react";
 import { connect } from "react-redux";
 import { getActivities } from "../store/activities";
 import Activity from "./Activity";
-// import L from "leaflet";
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import L from "leaflet";
+import {
+  MapContainer,
+  TileLayer,
+  Marker,
+  Popup,
+  Polyline,
+} from "react-leaflet";
+import polyUtil from "polyline-encoded";
 
 export const Activities = (props) => {
   const [activities, setActivities] = useState([]);
@@ -37,13 +44,26 @@ export const Activities = (props) => {
     return coords;
   }
 
+  console.log("activity 1 polyline ", props.activities);
+
+  const multiPolyline = props.activities
+    .filter((activity) => activity.distance !== 0)
+    .map((activity) => {
+      // console.log(polyUtil.decode(activity.map.summary_polyline));
+      return polyUtil.decode(activity.map.summary_polyline);
+    });
+
+  console.log(multiPolyline);
+
+  const limeOptions = { color: "lime" };
+
   getLocation();
   return (
     <div>
       <h3>Activities will be here</h3>
       <div id='map'>
         <MapContainer
-          center={[35.9563566, -84.0722167]}
+          center={[35.94, -84.0722167]}
           zoom={11}
           scrollWheelZoom={false}
         >
@@ -51,11 +71,12 @@ export const Activities = (props) => {
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
           />
-          <Marker position={[51.505, -0.09]}>
+          <Polyline pathOptions={limeOptions} positions={multiPolyline} />
+          {/* <Marker position={[51.505, -0.09]}>
             <Popup>
               A pretty CSS3 popup. <br /> Easily customizable.
             </Popup>
-          </Marker>
+          </Marker> */}
         </MapContainer>
       </div>
       <table>
